fc0f6a9a422469ee28d18b567be903a4d124a0f4
Roy-Ayalon <90352235+Roy-Ayalon@users.noreply.github.com>
2022-09-21 15:03:50 +0300

    Add files via upload

--- counter.v
+++ counter.v
@@ -8,18 +8,21 @@ parameter WIDTH = 8,
 parameter INC_SIZE = 1,
 parameter DEC_SIZE = 1
  )(
-input                   clk,
-input                   rst_n,
-input                   inc,
-input                   dec,
-output reg[WIDTH-1:0]   count
+input                       clk,
+input                       rst_n,
+input  reg[INC_SIZE-1:0]    inc,
+input  reg[INC_SIZE-1:0]    dec,
+output reg[WIDTH-1:0]       count
 );
 
 
 
 always @(posedge clk or negedge rst_n)
 if(~rst_n) count<= '0;
-else if (inc || dec) 
+else if (inc | dec) 
+if(inc)
 count <= count + {{WIDTH-1{1'b0}},{1'b1}};
+else
+count<= count-{{WIDTH-1{1'b0}},{1'b1}};
    
 endmodule
